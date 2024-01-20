cfcd657f24b0008efb385f9d7db43ddced95b95a
Monu Shaw <68293997+monu-shaw@users.noreply.github.com>
2024-01-20 09:56:10 +0000

    "weather ui"

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# portfolio
-Build Simple Portfolio Kind of site.
+# My Developer Portfolio Website
+This repository houses the source code for my personal portfolio website, built with , React, Vue, Javascript libraries]. Here, you can explore my professional journey, check out my projects, and learn more about my skills and qualifications.
 
 ## Live Link
 ['https://monu-shaw.github.io/portfolio/'](https://monu-shaw.github.io/portfolio)
--- assets/js/script.js
+++ assets/js/script.js
@@ -118,3 +118,375 @@ const showLess =()=>{
   );
   u("project").innerHTML = short+`<div class="col-10 col-md-6 col-lg-4 mx-auto my-2"><button class="btn btn-outline-warning rounded-pill w-100 smore" onclick="showAll()">Show More </button></div>`
 }
+
+// Show Alerts 
+const TriggerMessage = (message,type)=>{
+  const alertElement = document.querySelector('.alert');
+    const alertMessage = document.querySelector('#alertMessage');
+    alertMessage.text = message
+
+    alertElement.classList.add(`alert-${type}`,'show');
+}
+const Alert ={
+  success:(message)=>{
+    TriggerMessage(message,'success')
+  },
+  error:(message)=>{
+    TriggerMessage(message,'error')
+  },
+}
+
+//Weather 
+window.onload = function() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, handleError);
+  } else {
+    // Geolocation is not supported by this browser
+    handleError("Geolocation is not supported by this browser.");
+  }
+};
+
+async function showPosition(position) {
+  let latitude =  position.coords.latitude
+  let longitude=  position.coords.longitude
+
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${'e08ecf922a7d4742624f80d4fa6e07c8'}`);
+    const data = await response.json();
+
+    const city = data.name;
+    const temperature = Math.floor(+data.main.temp-273.15);
+    const description = data.weather[0].description;
+    const mainWeather = data.weather[0].main;
+    const pressure = data.main.pressure
+    const humidity = data.main.humidity
+    const speed = data.wind.speed
+    const country = data.sys.country
+
+    document.getElementById('custCard').classList.remove('d-none')
+    // Example usage: Displaying weather information
+    console.log(data);
+    setInputValueById("mainWeather", mainWeather);
+    setInputValueById("temp", temperature+"°C");
+    setInputValueById("pressure", pressure+" hPa");
+    setInputValueById("humidity", humidity+"%");
+    setInputValueById("speed", speed+" km/h");
+    setInputValueById("location", `${city} , ${countryCode?.[country]?countryCode?.[country]:' '}`);
+    changeWeatherIcon(data?.weather?.[0].main?data?.weather?.[0].main:'')
+
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
+}
+
+function handleError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      Alert.error("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      Alert.error("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      Alert.error("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      Alert.error("An unknown error occurred.");
+      break;
+  }
+}
+
+function changeWeatherIcon(weatherCondition) {
+  const weatherIconMap = {
+    "clear sky": "sunny.svg",
+    "clear": "sunny.svg",
+      rain: "rainy.svg",
+    "few clouds": "cloudy.svg",
+      snow: "snow.svg",
+      thunderstorm: "thunder.svg",
+  };
+
+  const weatherBackgroundMap = {
+    "clear sky": "#fff8ccb9",
+    "clear": "#fff8ccb9",
+      rain: "lightgray",
+    "few clouds": "silver",
+      snow: "f5f5f571",
+      thunderstorm: "darkgray",
+  };
+  const background = weatherBackgroundMap[weatherCondition.toLowerCase()] || "lightblue";
+
+  const icon = weatherIconMap[weatherCondition.toLowerCase()] || "unknown.png";
+  
+  const imgElement = document.getElementById("weatherIcon");
+  imgElement.src = "./assets/image/weather/" + icon;
+
+  const divElement = document.getElementById("upper");
+  divElement.style.backgroundColor = background;
+}
+
+
+
+function setInputValueById(id, value) {
+  const inputElement = document.getElementById(id);
+
+  if (inputElement) {
+    inputElement.innerText = value;
+  } else {
+    console.error(`Input element with ID "${id}" not found.`);
+  }
+}
+
+
+const countryCode = {
+    "AD": "Andorra",
+    "AE": "United Arab Emirates",
+    "AF": "Afghanistan",
+    "AG": "Antigua and Barbuda",
+    "AI": "Anguilla",
+    "AL": "Albania",
+    "AM": "Armenia",
+    "AO": "Angola",
+    "AQ": "Antarctica",
+    "AR": "Argentina",
+    "AS": "American Samoa",
+    "AT": "Austria",
+    "AU": "Australia",
+    "AW": "Aruba",
+    "AX": "Åland Islands",
+    "AZ": "Azerbaijan",
+    "BA": "Bosnia and Herzegovina",
+    "BB": "Barbados",
+    "BD": "Bangladesh",
+    "BE": "Belgium",
+    "BF": "Burkina Faso",
+    "BG": "Bulgaria",
+    "BH": "Bahrain",
+    "BI": "Burundi",
+    "BJ": "Benin",
+    "BL": "Saint Barthélemy",
+    "BM": "Bermuda",
+    "BN": "Brunei Darussalam",
+    "BO": "Bolivia (Plurinational State of)",
+    "BQ": "Bonaire, Sint Eustatius and Saba",
+    "BR": "Brazil",
+    "BS": "Bahamas",
+    "BT": "Bhutan",
+    "BV": "Bouvet Island",
+    "BW": "Botswana",
+    "BY": "Belarus",
+    "BZ": "Belize",
+    "CA": "Canada",
+    "CC": "Cocos (Keeling) Islands",
+    "CD": "Congo, the Democratic Republic of the",
+    "CF": "Central African Republic",
+    "CG": "Congo",
+    "CH": "Switzerland",
+    "CI": "Côte d'Ivoire",
+    "CK": "Cook Islands",
+    "CL": "Chile",
+    "CM": "Cameroon",
+    "CN": "China",
+    "CO": "Colombia",
+    "CR": "Costa Rica",
+    "CU": "Cuba",
+    "CV": "Cabo Verde",
+    "CW": "Curaçao",
+    "CX": "Christmas Island",
+    "CY": "Cyprus",
+    "CZ": "Czech Republic",
+    "DE": "Germany",
+    "DJ": "Djibouti",
+    "DK": "Denmark",
+    "DM": "Dominica",
+    "DO": "Dominican Republic",
+    "DZ": "Algeria",
+    "EC": "Ecuador",
+    "EE": "Estonia",
+    "EG": "Egypt",
+    "EH": "Western Sahara",
+    "ER": "Eritrea",
+    "ES": "Spain",
+    "ET": "Ethiopia",
+    "FI": "Finland",
+    "FJ": "Fiji",
+    "FK": "Falkland Islands (Malvinas)",
+    "FM": "Micronesia (Federated States of)",
+    "FO": "Faroe Islands",
+    "FR": "France",
+    "GA": "Gabon",
+    "GB": "United Kingdom of Great Britain and Northern Ireland",
+    "GD": "Grenada",
+    "GE": "Georgia",
+    "GF": "French Guiana",
+    "GG": "Guernsey",
+    "GH": "Ghana",
+    "GI": "Gibraltar",
+    "GL": "Greenland",
+    "GM": "Gambia",
+    "GN": "Guinea",
+    "GP": "Guadeloupe",
+    "GQ": "Equatorial Guinea",
+    "GR": "Greece",
+    "GS": "South Georgia and the South Sandwich Islands",
+    "GT": "Guatemala",
+    "GU": "Guam",
+    "GW": "Guinea-Bissau",
+    "GY": "Guyana",
+    "HK": "Hong Kong",
+    "HN": "Honduras",
+    "HR": "Croatia",
+    "HT": "Haiti",
+    "HU": "Hungary",
+    "ID": "Indonesia",
+    "IE": "Ireland",
+    "IL": "Israel",
+    "IM": "Isle of Man",
+    "IN": "India",
+    "IQ": "Iraq",
+    "IR": "Iran (Islamic Republic of)",
+    "IS": "Iceland",
+    "IT": "Italy",
+    "JE": "Jersey",
+    "JM": "Jamaica",
+    "JO": "Jordan",
+    "JP": "Japan",
+    "KE": "Kenya",
+    "KG": "Kyrgyzstan",
+    "KH": "Cambodia",
+    "KI": "Kiribati",
+    "KM": "Comoros",
+    "KN": "Saint Kitts and Nevis",
+    "KP": "Korea (Democratic People's Republic of)",
+    "KR": "Korea, Republic of",
+    "KW": "Kuwait",
+    "KY": "Cayman Islands",
+    "KZ": "Kazakhstan",
+    "LA": "Lao People's Democratic Republic",
+    "LB": "Lebanon",
+    "LC": "Saint Lucia",
+    "LI": "Liechtenstein",
+    "LK": "Sri Lanka",
+    "LR": "Liberia",
+    "LS": "Lesotho",
+    "LT": "Lithuania",
+    "LU": "Luxembourg",
+    "LV": "Latvia",
+    "LY": "Libya",
+    "MA": "Morocco",
+    "MC": "Monaco",
+    "MD": "Moldova, Republic of",
+    "ME": "Montenegro",
+    "MF": "Saint Martin (French part)",
+    "MG": "Madagascar",
+    "MH": "Marshall Islands",
+    "MK": "North Macedonia",
+    "ML": "Mali",
+    "MM": "Myanmar",
+    "MN": "Mongolia",
+    "MO": "Macao",
+    "MP": "Northern Mariana Islands",
+    "MQ": "Martinique",
+    "MR": "Mauritania",
+    "MS": "Montserrat",
+    "MT": "Malta",
+    "MU": "Mauritius",
+    "MV": "Maldives",
+    "MW": "Malawi",
+    "MX": "Mexico",
+    "MY": "Malaysia",
+    "MZ": "Mozambique",
+    "NA": "Namibia",
+    "NC": "New Caledonia",
+    "NE": "Niger",
+    "NF": "Norfolk Island",
+    "NG": "Nigeria",
+    "NI": "Nicaragua",
+    "NL": "Netherlands",
+    "NO": "Norway",
+    "NP": "Nepal",
+    "NR": "Nauru",
+    "NU": "Niue",
+    "NZ": "New Zealand",
+    "OM": "Oman",
+    "PA": "Panama",
+    "PE": "Peru",
+    "PF": "French Polynesia",
+    "PG": "Papua New Guinea",
+    "PH": "Philippines",
+    "PK": "Pakistan",
+    "PL": "Poland",
+    "PM": "Saint Pierre and Miquelon",
+    "PN": "Pitcairn",
+    "PR": "Puerto Rico",
+    "PS": "Palestine, State of",
+    "PT": "Portugal",
+    "PW": "Palau",
+    "PY": "Paraguay",
+    "QA": "Qatar",
+    "RE": "Réunion",
+    "RO": "Romania",
+    "RS": "Serbia",
+    "RU": "Russian Federation",
+    "RW": "Rwanda",
+    "SA": "Saudi Arabia",
+    "SB": "Solomon Islands",
+    "SC": "Seychelles",
+    "SD": "Sudan",
+    "SE": "Sweden",
+    "SG": "Singapore",
+    "SH": "Saint Helena, Ascension and Tristan da Cunha",
+    "SI": "Slovenia",
+    "SJ": "Svalbard and Jan Mayen",
+    "SK": "Slovakia",
+    "SL": "Sierra Leone",
+    "SM": "San Marino",
+    "SN": "Senegal",
+    "SO": "Somalia",
+    "SR": "Suriname",
+    "SS": "South Sudan",
+    "ST": "Sao Tome and Principe",
+    "SV": "El Salvador",
+    "SX": "Sint Maarten (Dutch part)",
+    "SY": "Syrian Arab Republic",
+    "SZ": "Eswat",
+    "TC": "Turks and Caicos Islands",
+  "TD": "Chad",
+  "TF": "French Southern Territories",
+  "TG": "Togo",
+  "TH": "Thailand",
+  "TJ": "Tajikistan",
+  "TK": "Tokelau",
+  "TL": "Timor-Leste",
+  "TM": "Turkmenistan",
+  "TN": "Tunisia",
+  "TO": "Tonga",
+  "TR": "Turkey",
+  "TT": "Trinidad and Tobago",
+  "TV": "Tuvalu",
+  "TW": "Taiwan, Province of China",
+  "TZ": "Tanzania, United Republic of",
+  "UA": "Ukraine",
+  "UG": "Uganda",
+  "UM": "United States Minor Outlying Islands",
+  "US": "United States of America",
+  "UY": "Uruguay",
+  "UZ": "Uzbekistan",
+  "VA": "Holy See",
+  "VC": "Saint Vincent and the Grenadines",
+  "VE": "Venezuela (Bolivarian Republic of)",
+  "VG": "Virgin Islands (British)",
+  "VI": "Virgin Islands (U.S.)",
+  "VN": "Viet Nam",
+  "VU": "Vanuatu",
+  "WF": "Wallis and Futuna",
+  "WS": "Samoa",
+  "YE": "Yemen",
+  "YT": "Mayotte",
+  "ZA": "South Africa",
+  "ZM": "Zambia",
+  "ZW": "Zimbabwe"
+}
+const closeWeather=()=>{
+  document.getElementById('custCard').classList.add('d-none')
+}
